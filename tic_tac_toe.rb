715d779f2633549183f6576bd3fffcd7f8290435
David O'Mullan <35239651+davidomullan@users.noreply.github.com>
2023-09-11 09:56:57 -0500

    Freeze constant LINES as recommended by style guide

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -35,7 +35,7 @@ end
 
 # Game Class
 class Game
-  LINES = [%w[1 2 3], %w[4 5 6], %w[7 8 9], %w[1 4 7], %w[2 5 8], %w[3 6 9], %w[1 5 9], %w[3 5 7]]
+  LINES = [%w[1 2 3], %w[4 5 6], %w[7 8 9], %w[1 4 7], %w[2 5 8], %w[3 6 9], %w[1 5 9], %w[3 5 7]].freeze
   attr_accessor :board_state, :current_player, :win_condition
 
   def initialize(first_player)
